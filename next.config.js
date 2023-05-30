29e99dc7b22b383c236e7b89487ac4ba01388469
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2023-05-30 12:57:28 +0700

    ubah link

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "drive.google.com",
+        hostname: "photos.google.com",
         port: "",
       },
     ],
